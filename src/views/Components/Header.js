5439900041135064fe1a3b428732f073adbbca6e
Jaspher040696 <jaspher040696@gmail.com>
2024-02-17 16:43:49 +0800

    update

--- src/views/Components/Header.js
+++ src/views/Components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import SchoolLogo from "../../assets/images/SDC.png";
 import { Button } from "react-bootstrap";
-import Logout from "../../assets/images/icons/logout.svg"
+import Logout from "../../assets/images/icons/logout.svg";
+import MENU from "../../assets/images/icons/bars-solid.svg"
 
 export default function Header() {
   const logout = async () => {
@@ -11,13 +12,14 @@ export default function Header() {
 
   return (
     <div className="header mb-2">
+      <img className="menu" src={MENU} alt="" />
       <div className="logo">
         <img className="img" src={SchoolLogo} alt="" />
         <h2 className="title">PROJECT IN OUT</h2>
       </div>
-      <div className="lg-container" onClick={logout}>
+      {/* <div className="lg-container" onClick={logout}>
         <img className="logout" src={Logout} alt="" />Logout
-      </div>
+      </div> */}
     </div>
   );
 }
