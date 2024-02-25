e2a14181c8b0a6adcb36a8988fa8b71294309bb1
unknown <placia.kent@gmail.com>
2024-02-26 05:24:10 +0800

    fix101

--- src/components/SideBarData.js
+++ src/components/SideBarData.js
@@ -18,26 +18,10 @@ export const SideBarData = [
   },
   {
     title: "Departments",
+    path: '/divisions',
     icon: <FontAwesomeIcon icon={faSitemap} />,
     iconClosed: <FontAwesomeIcon icon={faAngleDown} />,
     iconOpened: <FontAwesomeIcon icon={faAngleUp} />,
-    subNav: [
-      {
-        title: "Department 1",
-        path: "/",
-        icon: <span>&#9900;</span>,
-      },
-      {
-        title: "Department 2",
-        path: "/",
-        icon: <span>&#9900;</span>,
-      },
-      {
-        title: "Department 3",
-        path: "/collegedepartment",
-        icon: <span>&#9900;</span>,
-      }
-    ],
   },
   {
     title: "Reports ",
