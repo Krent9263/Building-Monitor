9b3720c52e2a3658963994448545e708ee3b9df8
unknown <placia.kent@gmail.com>
2024-02-29 22:34:47 +0800

    fix delete

--- src/views/employee/Employee.js
+++ src/views/employee/Employee.js
@@ -68,22 +68,22 @@ function Employee() {
     }
   };
 
-  const handleDeleteEmployee = () => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#35482e",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-      if (result.isConfirmed) {
-        console.log("Deleting employee...");
-        deleteUserAccount();
-      }
-    });
-  };
+  // const handleDeleteEmployee = () => {
+  //   Swal.fire({
+  //     title: "Are you sure?",
+  //     text: "You won't be able to revert this!",
+  //     icon: "warning",
+  //     showCancelButton: true,
+  //     confirmButtonColor: "#35482e",
+  //     cancelButtonColor: "#d33",
+  //     confirmButtonText: "Yes, delete it!"
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+  //       console.log("Deleting employee...");
+  //       deleteUserAccount();
+  //     }
+  //   });
+  // };
 
   const getAllUserAccountByDivisionIdAndOfficeId = async () => {
     let response = await new UserAccountAPI().getAllUserAccountByDivisionIdAndOfficeId(officeId, divisionId);
@@ -163,7 +163,7 @@ function Employee() {
         confirmBtnText="Yes, delete it!"
         confirmBtnBsStyle="danger"
         title="Are you sure?"
-        onConfirm={handleDeleteEmployee}
+        onConfirm={deleteUserAccount}
         onCancel={cancelSweetAlert}
         focusCancelBtn
       >
